562a1b3a0140f47898cc6aaf00581843e99fbf05
Oleg Khavroshin <okhavroshin@mirantis.com>
2016-01-25 00:29:43 +0300

    kv file added.

--- main.py
+++ main.py
@@ -4,14 +4,48 @@
 """
 Интерфейсная часть
 """
-
-import Kivy
 import PocketClass
+from kivy.app import App, Builder
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.graphics import Rectangle, Color #, Canvas
 
-pcs = PocketClass.Pockets
 
-class My_Face():
-    def __int__(self):
-        pass
+class MyFace(BoxLayout):
+    item_in = ObjectProperty(None)
+    item_out = ObjectProperty(None)
 
-pass
+
+    def get_last(self, item_name):
+        """
+        :param item_name:
+        :return: возврат последнего использованного значения этой позиции
+        """
+        #todo возврат последнего
+        return item_name
+
+
+class ItemsIn(BoxLayout):
+    pass
+
+
+class ItemsOut(BoxLayout):
+    pass
+
+
+class MyMoney(App):
+    def build(self):
+        face = MyFace()
+#        item_in = Items_in()
+#        item_out = Items_out()
+#        face.add_widget(item_in)
+#        face.add_widget(item_out)
+        return face
+
+
+if __name__ == '__main__':
+    Builder.load_file('main.kv')
+    MyMoney().run()
