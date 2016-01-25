88dea1369a4c5b57a1ace58d632aee6527db8a20
Oleg Khavroshin <okhavroshin@mirantis.com>
2016-01-25 18:51:47 +0300

    Interface is in progress

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
 from kivy.properties import NumericProperty, ObjectProperty, StringProperty
 from kivy.graphics import Rectangle, Color #, Canvas
-
+from functools import partial
 
 class MyFace(StackLayout):
-    item_in = ObjectProperty(None)
-    item_out = ObjectProperty(None)
+    # item_in = ObjectProperty(None)
+    # item_out = ObjectProperty(None)
+    # pocket = ObjectProperty(None)
+    # item = ObjectProperty(None)
+    # summ = ObjectProperty(None)
+    # amount = ObjectProperty(None)
+    # comment = ObjectProperty(None)
+    #
+    # btn = ObjectProperty(None)
 
     nu = NumericProperty(0)
 
@@ -31,8 +38,57 @@ class MyFace(StackLayout):
         #todo возврат последнего
         return item_name
 
-    def some_action(self):
-        if self.nu % 2 == 0:
+    def prepare_action(self, action_name):
+        if action_name == 'In':
+            self.pocket = DrpDwnList()
+            self.pocket.label_text = 'Кошелек:'
+            self.add_widget(self.pocket)
+            self.item_in = DrpDwnList()
+            self.item_in.label_text = 'Статья:'
+            self.add_widget(self.item_in)
+            self.summ = InptData()
+            self.summ.label_text = 'Сумма:'
+            self.add_widget(self.summ)
+            self.comment = InptData()
+            self.comment.label_text = 'Комментарий:'
+            self.add_widget(self.comment)
+            self.btn = Button(size_hint=(1, None), height=50,
+                              on_press=partial(self.some_action))
+            self.add_widget(self.btn)
+        elif action_name == 'Out':
+            self.pocket = DrpDwnList()
+            self.pocket.label_text = 'Кошелек:'
+            self.add_widget(self.pocket)
+            self.item_out = DrpDwnList()
+            self.item_out.label_text = 'Статья:'
+            self.add_widget(self.item_out)
+            self.summ = InptData()
+            self.summ.label_text = 'Сумма:'
+            self.add_widget(self.summ)
+            self.amount = InptData()
+            self.amount.label_text = 'Сумма:'
+            self.add_widget(self.amount)
+            self.comment = InptData()
+            self.comment.label_text = 'Комментарий:'
+            self.add_widget(self.comment)
+            self.btn = Button(size_hint=(1, None), height=50,
+                              on_press=partial(self.some_action))
+            self.add_widget(self.btn)
+        elif action_name == 'Betwean':
+            pass
+        elif action_name == 'Exchange':
+            pass
+        elif action_name == 'Credit1In':
+            pass
+        elif action_name == 'Credit1Out':
+            pass
+        elif action_name == 'Credit2In':
+            pass
+        elif action_name == 'Credit2Out':
+            pass
+
+    def some_action(self, x):
+        if self.nu % 2 == 1:
             self.item_in = DrpDwnList()
             self.item_out = DrpDwnList()
             self.add_widget(self.item_in)
@@ -42,15 +98,26 @@ class MyFace(StackLayout):
             self.nu += 1
         else:
             # self.clear_widgets()
+            # self.btn = Button(size_hint=(1, None), height=50,
+            #              on_press=self.some_action())
             self.remove_widget(self.item_in)
-            self.remove_widget(self.item_out)
+            # self.remove_widget(self.item_out)
+            # self.remove_widget(self.btn)
+            # self.add_widget(self.btn)
             self.nu += 1
+        # pass
 
 
-
-class DrpDwnList(StackLayout):
+class DrpDwnList(BoxLayout):
     label_text = StringProperty('')
     input_text = StringProperty('')
+    pass
+
+
+class InptData(BoxLayout):
+    label_text = StringProperty('')
+    input_text = StringProperty('')
+    input_summ = NumericProperty(0)
     pass
 
 
@@ -58,6 +125,7 @@ class MyMoney(App):
     def build(self):
 
         face = MyFace()
+        face.prepare_action('Out')
         # btn = Button()
         # face.add_widget(btn)
         return face
--- test.py
+++ test.py
@@ -65,52 +65,70 @@ print "\nafter filling:\n" + pcs.get_info()
 '''
 # pcs.close_db()
 
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.lang import Builder
+from kivy.graphics import Color, Rectangle
+from random import random as r
+from functools import partial
 
 
-Builder.load_string('''
-#:kivy 1.7.0
+class StressCanvasApp(App):
 
-<GridLayout>
-    canvas.before:
-        BorderImage:
-            # BorderImage behaves like the CSS BorderImage
-            border: 10, 10, 10, 10
-            source: '../examples/widgets/sequenced_images/data/images/button_white.png'
-            pos: self.pos
-            size: self.size
+    def add_rects(self, label, wid, count, *largs):
+        label.text = str(int(label.text) + count)
+        with wid.canvas:
+            for x in range(count):
+                Color(r(), 1, 1, mode='hsv')
+                Rectangle(pos=(r() * wid.width + wid.x,
+                               r() * wid.height + wid.y), size=(20, 20))
 
-<RootWidget>
-    GridLayout:
-        size_hint: .9, .9
-        pos_hint: {'center_x': .5, 'center_y': .5}
-        rows:1
-        Label:
-            text: "I don't suffer from insanity, I enjoy every minute of it"
-            text_size: self.width-20, self.height-20
-            valign: 'top'
-        Label:
-            text: "When I was born I was so surprised; I didn't speak for a year and a half."
-            text_size: self.width-20, self.height-20
-            valign: 'middle'
-            halign: 'center'
-        Label:
-            text: "A consultant is someone who takes a subject you understand and makes it sound confusing"
-            text_size: self.width-20, self.height-20
-            valign: 'bottom'
-            halign: 'justify'
-''')
+    def double_rects(self, label, wid, *largs):
+        count = int(label.text)
+        self.add_rects(label, wid, count, *largs)
 
-class RootWidget(FloatLayout):
-    pass
-
-
-class MainApp(App):
+    def reset_rects(self, label, wid, *largs):
+        label.text = '0'
+        wid.canvas.clear()
 
     def build(self):
-        return RootWidget()
+        wid = Widget()
+
+        label = Label(text='0')
+
+        btn_add100 = Button(text='+ 100 rects',
+                            on_press=partial(self.add_rects, label, wid, 100))
+
+        btn_add500 = Button(text='+ 500 rects',
+                            on_press=partial(self.add_rects, label, wid, 500))
+
+        btn_double = Button(text='x 2',
+                            on_press=partial(self.double_rects, label, wid))
+
+        btn_reset = Button(text='Reset',
+                           on_press=partial(self.reset_rects, label, wid))
+
+        btn_add0 = Button(text='+ 0 rects',
+                            on_press=partial(self.add_rects, label, wid, 0))
+
+        layout = BoxLayout(size_hint=(1, None), height=50)
+        layout.add_widget(btn_add100)
+        layout.add_widget(btn_add500)
+        layout.add_widget(btn_double)
+        layout.add_widget(btn_reset)
+        layout.add_widget(label)
+
+        layout2 = BoxLayout(size_hint=(1, None), height=50)
+        layout2.add_widget(btn_add0)
+
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(wid)
+        root.add_widget(layout)
+        root.add_widget(layout2)
+
+        return root
 
 if __name__ == '__main__':
-    MainApp().run()
+    StressCanvasApp().run()
